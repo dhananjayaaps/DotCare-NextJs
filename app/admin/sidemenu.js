dd41b3d2aed710321261e9110c6b33da4623debc
dhananjayaaps <dhananjayaaps@gmail.com>
2024-10-12 11:35:50 +0530

    changed side bar of admin

--- app/admin/sidemenu.js
+++ app/admin/sidemenu.js
@@ -84,6 +84,15 @@ const Sidemenu = ({ highlightedItem, loggedUser}) => {
             </li>
 
             <li>
+              <a
+                href="/admin/areas"
+                className={`block rounded-lg px-4 py-2 text-sm font-medium ${isActive('midwifes')} hover:bg-gray-100 hover:text-gray-700`}
+              >
+                Areas
+              </a>
+            </li>
+
+            <li>
               <details className="group [&_summary::-webkit-details-marker]:hidden">
                 <summary
                   className={`flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 ${isActive('Account')} hover:bg-gray-100 hover:text-gray-700`}
